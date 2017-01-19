b02df8f4e4c64a9c3b8385ba8d966a0e98e779c4
Kevin Kerr <kkerr1@gmail.com>
2017-01-18 22:35:04 -0800

    connect to elastic search

--- api/polls.js
+++ api/polls.js
@@ -1,11 +1,11 @@
 'use strict';
+const es = require('../db/es');
 
-exports.getPolls = (req, res) => {
-  const pollCollection = [
-		{ title: 'This is my first Question' },
-		{ title: 'This is my second Question' }
-  ];
-
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(pollCollection));
+exports.getPolls = (req, res, next) => {
+  return es.getNearbyPolls()
+    .then((polls) => {
+      res.setHeader('Content-Type', 'application/json'); // handle globally?
+      res.send(polls);
+    })
+    .catch(next); // TODO: Add Error Handler
 };
